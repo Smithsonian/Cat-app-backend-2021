30ca9330d3e2b6bc459ec6b0dd5ae7bfe910e712
Jorge Paul <jserranopaul@gmail.com>
2021-07-08 09:18:04 +0200

    Pagination and morgan

--- controllers/observationController.js
+++ controllers/observationController.js
@@ -2,6 +2,24 @@ import asyncHandler from '../middlewares/asyncHandler.js';
 import Observation from '../models/Observation.js';
 
 export const getObservations = asyncHandler(async (req, res) => {
-  const observations = await Observation.find();
-  res.status(200).json(observations);
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 100;
+  const startAt = (page - 1) * limit;
+  const endAt = page * limit;
+  const total = await Observation.countDocuments();
+  const pagination = {};
+  if (startAt > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    };
+  }
+  if (endAt < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+  const observations = await Observation.find().skip(startAt).limit(limit);
+  res.status(200).json({ pagination, observations });
 });
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 import cors from 'cors';
 import 'dotenv/config.js';
 import './db/mongoose.js';
@@ -8,7 +9,7 @@ import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(morgan('tiny'));
 app.use(cors({ origin: process.env.ORIGIN }));
 app.use('/auth', authRouter);
 app.use('/observations', observationRouter);
